2c54cce1ee9ae928f7d281c1ab9513901e5279e8
Felipe Guarnieri <guarnieri007@hotmail.com>
2022-02-17 21:02:41 -0300

    First commit on Git

---
+++ src/main/java/intro/Medidas.java
@@ -0,0 +1,37 @@
+/* 1- Pacote */
+package intro;
+/* 2- Referência as bibliotecas*/
+
+import java.sql.SQLOutput;
+
+/* 3- Classe */
+public class Medidas {
+    /* 3.1- Atributos - Caracteristicas
+    *  3.2- Métodos e Funções
+    * */
+
+    public static void main(String[] args)
+    {
+
+    }
+    public void calcularAreaModoExtenso()
+    {
+        System.out.println("Calculo de áreas!!!");
+
+        // cálculo de área - Exemplo: o tamanho do tapete ou do piso
+        int largura;
+        int comprimento;
+        int resultado;
+
+        largura = 5; //largura rece 4 unidades
+        comprimento = 3; //largura rece 3 unidades
+        resultado = largura * comprimento;
+        System.out.println("Para a largura de " + largura + "m² e o comprimento de " + comprimento + "m² a área é de " + resultado + "m².");
+    }
+    public void calcularAreaModoCompacto()
+    {
+        int largura = 4;
+        int comprimento = 3;
+        System.out.println("Para a largura de " + largura + "m² e o comprimento de " + comprimento + "m² a área é de " + largura * comprimento + "m².");
+    }
+}
